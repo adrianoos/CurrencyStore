54af8dc123815f8e4270cf31e8eeb7633911048d
Adrian <adriann.kowalski@gmail.com>
2021-04-27 20:41:21 +0200

    Create basic components structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import './App.css';
-import axios from 'axios';
-
 
 function App() {
-const table = 'C'
-const code = 'EUR'
-
-  const getTable = async () => {
-    try {
-     // const response = await axios.get(`http://api.nbp.pl/api/exchangerates/tables/${table}`)
-      const response = await axios.get(`http://api.nbp.pl/api/exchangerates/rates/${table}/${code}`)
-      //http://api.nbp.pl/api/exchangerates/rates/{table}/{code}/
-     // const data = response.JSON()
-      console.log(response)
-
-
-    } catch (error) {
-      
-    }
-  }
-getTable()
 
   return (
     <div className="App">
