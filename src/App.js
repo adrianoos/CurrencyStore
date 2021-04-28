7660cc7b223eab1d701b4fdb5260903da8990061
Adrian <adriann.kowalski@gmail.com>
2021-04-28 14:08:52 +0200

    added nav button and actions for fetching second type of table A or B

--- src/App.js
+++ src/App.js
@@ -3,27 +3,39 @@ import './App.css';
 import { fetchData, Header, Navigation, CurrenciesList } from './Components';
 
 
-
 const App = () => {
 
 const [ data, setData ] = useState({})
-
-const tableType = 'A' // temporary value
+const [ tableType, setTableType ] = useState('A')
 
 const getData = async () =>{
 const response = await fetchData(tableType)
 setData(response)
 }
 
+console.log(data)
+
 useEffect(() => {
   getData()
- },[])
+ }, [tableType])
+
+ const changeTable = () => {
+   if (tableType === 'A') {
+     setTableType('B')
+   } else if (tableType === 'B') {
+     setTableType('A')
+   }
+ }
+
 
 
   return (
     <div className="App">
       <Header data={data}/>
-      <Navigation />
+      <Navigation
+      changeTable={changeTable}
+      tableType={tableType}
+      />
       <CurrenciesList data={data}/>
     </div>
   );
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Navigation = () => {
+const Navigation = ({ tableType, changeTable }) => {
+
+
     return (
         <div className='Navigation'>
             <button className='NavButtons'>Favorites</button>
             <button className='NavButtons'>Delete All</button>
+            { tableType === 'A' ? <button className='NavButtons' onClick={() => changeTable()}>Table A</button> :
+            <button className='NavButtons' onClick={() => changeTable()}>Table B</button>
+            }
+            
         </div>
     )
 }
