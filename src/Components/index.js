76f3fe5c58c08d154bc273780363835bb09d8f77
Adrian Kowalski <adriann.kowalski@gmail.com>
2021-05-04 09:18:14 +0200

    Delete src directory

--- src/Components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as fetchData } from './Api';
-export { default as Header } from './Header';
-export { default as Navigation } from './Navigation';
-export { default as CurrenciesList } from './CurrenciesList';
-export { default as CurrencyItem } from './CurrencyItem';
-
-
-
--- src/Reducers/favsDisplay.js
+++
@@ -1,9 +0,0 @@
-const favsDisplay = ( state = false , action ) => {
-    switch (action.type) {
-    case 'setFavs' :
-    return !state
-    default: 
-    return state
-    }}
-
-    export default favsDisplay;
